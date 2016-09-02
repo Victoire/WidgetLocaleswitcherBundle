fc88b35936eb9b3050b986acfb75db3099a57688
Leny Bernard <leny@troopers.email>
2016-09-02 11:59:57 +0200

    support extra localized links to be more flexible with non managed locales

--- DependencyInjection/VictoireWidgetLocaleSwitcherExtension.php
+++ DependencyInjection/VictoireWidgetLocaleSwitcherExtension.php
@@ -24,5 +24,7 @@ class VictoireWidgetLocaleSwitcherExtension extends Extension
 
         $loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
         $loader->load('services.yml');
+
+        $container->setParameter('locale_switcher.extraLocalizedLinks', $config['extraLocalizedLinks']);
     }
 }
